f3ab931e08bafc6d0aa79b4bef357c0b34e78c7e
Khaleeq <khaleeq.tanweer@gmail.com>
2022-08-12 11:21:59 +0100

    Shopping website

--- src/test/java/DemoTest.java
+++ src/test/java/DemoTest.java
@@ -12,7 +12,6 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class DemoTest {
 
-
     private RemoteWebDriver driver;
 
     @BeforeEach
@@ -22,19 +21,19 @@ public class DemoTest {
 
     @Test
     void demoTest() {
-        this.driver.get("https://www.bbc.co.uk/search?q="); // goes to the page
-        WebElement search = this.driver.findElement(By.cssSelector("#search-input")); // selects the search input
-        search.sendKeys("Turtles"); // types in "turtles"
+        this.driver.get("http://automationpractice.com/index.php"); // goes to the page
+        WebElement search = this.driver.findElement(By.cssSelector("#search_query_top")); // selects the search input
+        search.sendKeys("Dress"); // types in "turtles"
         search.sendKeys(Keys.ENTER); // submits using the enter key
 //		this.driver.findElement(By.cssSelector("#main-content > div:nth-child(2) > form > div > div > div > button"))
 //				.click();
         // First result in the results page:
         WebElement result = this.driver.findElement(By.cssSelector(
-                "#main-content > div:nth-child(3) > div.ssrcss-1v7bxtk-StyledContainer.enjd40x0 > div > div > ul > li:nth-child(1) > div > div > div.ssrcss-tq7xfh-PromoContent.e1f5wbog7 > div.ssrcss-1f3bvyz-Stack.e1y4nx260 > a > span > p > span"));
+                "#center_column > ul > li:nth-child(1) > div > div.right-block > h5 > a"));
 
-//		search.getRect();  lol
+        //		search.getRect();  lol
         // comparing the text of the result to the expected value
-        assertEquals("The Man Who Made Mock Turtle Soup", result.getText());
+        assertEquals("Printed Summer Dress", result.getText());
 
     }
 
