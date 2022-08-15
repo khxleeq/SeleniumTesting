ba124423d91b8fe2ae79bfcf9c97caa4c86345cc
Khaleeq <khaleeq.tanweer@gmail.com>
2022-08-15 16:24:20 +0100

    Shopping website bonus

--- src/test/java/ftseExercise.java
+++ src/test/java/ftseExercise.java
@@ -1,4 +1,6 @@
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
@@ -9,5 +11,12 @@ public class ftseExercise {
     @BeforeEach
     void setup() {
         this.driver = new ChromeDriver();
+        driver.manage().window().setSize(new Dimension(1280, 720));
+    }
+
+    @Test
+    void demoTest() {
+        this.driver.get("https://www.hl.co.uk/shares/stock-market-summary/ftse-100/risers");
+        webElement top
     }
 }
